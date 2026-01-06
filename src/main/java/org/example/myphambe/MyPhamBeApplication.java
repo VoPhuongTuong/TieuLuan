c9f8bda5370c810155a562614a79e614a5cf391d
Võ Phượng Tường <22130316@st.hcmuaf.edu.vn>
2026-01-06 21:34:13 +0700

    thumuc

---
+++ src/main/java/org/example/myphambe/MyPhamBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.myphambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPhamBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/myphambe/MyPhamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myphambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
